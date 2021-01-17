5b0c7a68c9d5181a009da4173a6de3ab3075018f
João Vitor Dias <jvitordiass@outlook.com>
2021-01-17 14:58:24 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes.js'
 
 const port = process.env.PORT || 3001
@@ -6,5 +7,6 @@ const port = process.env.PORT || 3001
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 app.listen(port, () => console.log('> Server is running...'))
